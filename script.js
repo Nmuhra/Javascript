96b436fc021bf2b71ff8dd5d334def8d2a2c3c15
Nabegh <nabeghmuhra@nais.co.za>
2022-08-19 08:43:12 +0200

    .

---
+++ script.js
@@ -0,0 +1,6 @@
+function on() {
+    document.getElementById('myImage').src='https://imgs.search.brave.com/FstGyZPyxphPr0KR80JsTZvzjZ8tMZssY2LdVSzz9P4/rs:fit:1200:1200:1/g:ce/aHR0cDovL3d3dy5w/bmdwaXguY29tL3dw/LWNvbnRlbnQvdXBs/b2Fkcy8yMDE2LzEw/L1BOR1BJWC1DT00t/TGlnaHQtQnVsYi1W/ZWN0b3ItUE5HLVRy/YW5zcGFyZW50LUlt/YWdlLnBuZw'
+}
+function off() {
+    document.getElementById('myImage').src='https://imgs.search.brave.com/W4NLYcOnEm7Xe6u1qcUd0UxYTKENInShJrMD295eBCs/rs:fit:778:1200:1/g:ce/aHR0cDovL2NsaXBh/cnQtbGlicmFyeS5j/b20vaW1hZ2VzX2sv/bGlnaHQtYnVsYi10/cmFuc3BhcmVudC1w/bmcvbGlnaHQtYnVs/Yi10cmFuc3BhcmVu/dC1wbmctNy5wbmc'
+}
